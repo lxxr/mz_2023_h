31cbd1bcf314676e9cdae92a4c671238977130d8
lxxr <lexx@rcbd.org>
2023-05-28 22:37:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import time  # to simulate a real time data, time loop
 import numpy as np  # np mean, np random
-import plotly.express as px  # interactive charts
 import streamlit as st
 import pandas as pd
 from io import StringIO
